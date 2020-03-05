25543115d73d9bd5e136cb21b67a45ee510e9c67
Schulte,D. (Douwe) <d.schulte@students.uu.nl>
2020-03-05 16:03:44 +0100

    Implemented #25

--- source/Structs/ProgressBar.cs
+++ source/Structs/ProgressBar.cs
@@ -1,15 +1,21 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
 
 namespace AssemblyNameSpace
 {
     public class ProgressBar
     {
+        object ValueKey = new object();
         int max_value;
         int current_value = 0;
         Stopwatch stopwatch;
         bool free = true;
+        Timer timer;
+        int interval = 1000;
+        long estimate = 0;
+        long lasttask = 0;
 
         public ProgressBar()
         {
@@ -21,16 +27,35 @@ namespace AssemblyNameSpace
             max_value = max;
             stopwatch.Start();
             Draw(false);
+            timer = new Timer(Tick, null, interval, Timeout.Infinite);
         }
 
         public void Update(int add = 1)
         {
-            current_value += add;
-            if (current_value == max_value)
+            lock (ValueKey)
             {
-                stopwatch.Stop();
+                current_value += add;
+                lasttask = stopwatch.ElapsedMilliseconds;
+
+                if (current_value == max_value)
+                {
+                    stopwatch.Stop();
+                }
             }
             Draw();
+        }
+
+        private void Tick(object state)
+        {
+            try
+            {
+                Draw();
+            }
+            finally
+            {
+                interval = (int)(interval * 1.1);
+                timer?.Change(interval, Timeout.Infinite);
+            }
         }
 
         void Draw(bool clear = true)
@@ -58,15 +83,47 @@ namespace AssemblyNameSpace
                     width = 30;
                 }
 
-                var tail = $"| {Math.Round((double)current_value / max_value * 100),3}% {HelperFunctionality.DisplayTime(stopwatch.ElapsedMilliseconds)} ";
+                int value;
+                lock (ValueKey)
+                {
+                    value = current_value;
+                }
+
+                var tail = $"| {Math.Round((double)value / max_value * 100),3}% {HelperFunctionality.DisplayTime(stopwatch.ElapsedMilliseconds)} Left ~{HelperFunctionality.DisplayTime(GetEstimate())}";
                 var barlength = width - tail.Length - 1;
-                var position = (int)Math.Round((double)current_value / max_value * barlength);
+                var position = (int)Math.Round((double)value / max_value * barlength);
                 var stem = new String('-', position);
                 var empty = new String(' ', barlength - position);
                 Console.Write($"{stem}>{empty}{tail}");
 
                 free = true;
             }
+        }
+
+        long GetEstimate()
+        {
+            int value;
+            lock (ValueKey)
+            {
+                value = current_value;
+            }
+
+            long current_estimate;
+            try
+            {
+                current_estimate = (long)Math.Round((double)lasttask / value * (max_value - value) - (stopwatch.ElapsedMilliseconds - lasttask) * 0.6);
+                current_estimate = Math.Max(0, current_estimate);
+            }
+            catch
+            {
+                current_estimate = 0;
+            }
+
+            estimate = estimate == 0 ? current_estimate : (long)(estimate * 0.7 + current_estimate * 0.3);
+
+            if (value == max_value) estimate = 0;
+
+            return estimate;
         }
     }
 }
