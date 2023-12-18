1485ae02a71338f3ca9da8092fa8673684d21345
Douwe Schulte <d.schulte@uu.nl>
2023-12-18 15:12:35 +0100

    Added depth of coverage to fasta output

--- stitch/Reporting/FASTAReport.cs
+++ stitch/Reporting/FASTAReport.cs
@@ -13,8 +13,8 @@ namespace Stitch {
         /// <param name="parameters">The parameters.</param>
         /// <param name="min_score">The minimal score needed to be included in the file.</param>
         public FASTAReport(ReportInputParameters parameters, int min_score, RunParameters.Report.OutputType outputType, int maxThreads) : base(parameters, maxThreads) {
-            MinScore = min_score;
-            OutputType = outputType;
+            this.MinScore = min_score;
+            this.OutputType = outputType;
         }
 
         /// <summary> Creates a FASTA file with a score for each path through the graph. The lines will be sorted and the lines can be filtered for a minimal score. </summary>
@@ -22,19 +22,23 @@ namespace Stitch {
         public override string Create() {
             var sequences = new List<(double, string)>();
 
-            if (OutputType == RunParameters.Report.OutputType.Recombine) {
-                sequences.EnsureCapacity(Parameters.RecombinedSegment.Select(a => a.Templates.Count).Sum());
-                foreach (var template in Parameters.RecombinedSegment.SelectMany(a => a.Templates)) {
-                    if (template.Score >= MinScore)
-                        sequences.Add((template.Score, $">{template.MetaData.Identifier} score:{template.Score}\n{AminoAcid.ArrayToString(template.ConsensusSequence().Item1.SelectMany(i => i.Sequence))}"));
+            if (this.OutputType == RunParameters.Report.OutputType.Recombine) {
+                sequences.EnsureCapacity(this.Parameters.RecombinedSegment.Select(a => a.Templates.Count).Sum());
+                foreach (var template in this.Parameters.RecombinedSegment.SelectMany(a => a.Templates)) {
+                    if (template.Score >= this.MinScore) {
+                        var doc = System.String.Join(',', template.ConsensusSequence().Item2.Select(i => i.ToString("F2")));
+                        sequences.Add((template.Score, $">{template.MetaData.Identifier} score:{template.Score} depth_of_coverage:{doc}\n{AminoAcid.ArrayToString(template.ConsensusSequence().Item1.SelectMany(i => i.Sequence))}"));
+                    }
                 }
             } else // TemplateMatching
               {
-                sequences.EnsureCapacity(Parameters.Groups.Select(a => a.Item2.Count).Sum());
-                foreach (var (group, dbs) in Parameters.Groups) {
+                sequences.EnsureCapacity(this.Parameters.Groups.Select(a => a.Segments.Count).Sum());
+                foreach (var (group, dbs) in this.Parameters.Groups) {
                     foreach (var template in dbs.SelectMany(a => a.Templates)) {
-                        if (template.Score >= MinScore)
-                            sequences.Add((template.Score, $">{template.MetaData.Identifier} id:{group}-{template.Location.TemplateIndex} score:{template.Score}\n{AminoAcid.ArrayToString(template.ConsensusSequence().Item1.SelectMany(i => i.Sequence))}"));
+                        if (template.Score >= this.MinScore) {
+                            var doc = System.String.Join(',', template.ConsensusSequence().Item2.Select(i => i.ToString("F2")));
+                            sequences.Add((template.Score, $">{template.MetaData.Identifier} id:{group}-{template.Location.TemplateIndex} score:{template.Score} depth_of_coverage:{doc}\n{AminoAcid.ArrayToString(template.ConsensusSequence().Item1.SelectMany(i => i.Sequence))}"));
+                        }
                     }
                 }
             }
